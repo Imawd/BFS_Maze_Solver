ffa31716fb07f5d5476fa3553052db9915696931
Imaad Feerasta <feerasta@usc.edu>
2022-10-18 17:26:56 -0700

    FINAL

--- queue.cpp
+++ queue.cpp
@@ -6,7 +6,6 @@ queue.cpp
 
 //Constructor. maxlen must be as large as the total number
 // of Locations that will ever be entered into this Queue.
-// This is complete, you don't need to change it.
 Queue::Queue(int maxlen) {
    contents = new Location[maxlen];
    head = 0;
@@ -14,7 +13,6 @@ Queue::Queue(int maxlen) {
 }
 
 //Destructor. releases resources. C++ will call it automatically.
-//This is complete, you don't need to change it.
 Queue::~Queue() {
   delete[] contents;
 }
@@ -22,19 +20,16 @@ Queue::~Queue() {
 //Insert a new Location at the end/back of our list   
 void Queue::add_to_back(Location loc) {
 	
-   // *** You complete **** CHECKPOINT 3
    contents[tail++] = loc;
 }
 
 //Return and "remove" the oldest Location not already extracted
 Location Queue::remove_from_front() {
   
-   // *** You complete **** CHECKPOINT 3
    return contents[head++];
 }
 
 //Is this Queue empty? (did we extract everything added?)
-//This is complete, you don't need to change it.
 bool Queue::is_empty() {
    return head == tail;
 }
